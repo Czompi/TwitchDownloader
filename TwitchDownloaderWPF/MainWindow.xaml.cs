9274ecba278f0f43dd3c25a3d27e60b8b6afdf2a
Dávid Czompó <czompi@czompi.hu>
2023-03-16 23:18:36 +0100

    Bump app version

--- TwitchDownloaderWPF/MainWindow.xaml.cs
+++ TwitchDownloaderWPF/MainWindow.xaml.cs
@@ -75,7 +75,7 @@ namespace TwitchDownloaderWPF
             if (!File.Exists("ffmpeg.exe"))
                 await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Full);
 
-            Version currentVersion = new Version("1.52.1");
+            Version currentVersion = new Version("1001.52.1");
             Title = $"Twitch Downloader v{currentVersion}";
             AutoUpdater.InstalledVersion = currentVersion;
 #if !DEBUG
